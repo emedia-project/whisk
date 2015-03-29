5e1ed64e1f2d5d9c13b65ccc305dcfe6a7f0228f
glejeune <gregoire.lejeune@free.fr>
2015-03-29 18:29:31 +0200

    first commit

---
+++ src/whisk.erl
@@ -0,0 +1,168 @@
+-module(whisk).
+-behaviour(gen_server).
+-include("../include/whisk.hrl").
+-define(SERVER, ?MODULE).
+-define(TCP_OPTS, [
+                   binary, {packet, raw}, {nodelay, true},{reuseaddr, true}, {active, true}
+                  ]).
+-define(TIMEOUT, 5000).
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([
+         start_link/2,
+         connect/2,
+         disconnect/0,
+         version/0,
+         stat/0,
+         set/2,
+         set/3,
+         get/1,
+         delete/1
+        ]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link(Host, Port) ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [Host, Port], []).
+
+connect(Host, Port) ->
+  start_link(Host, Port).
+
+disconnect() ->
+  gen_server:cast(?SERVER, stop).
+
+version() ->
+  gen_server:call(?SERVER, version).
+
+stat() ->
+  gen_server:call(?SERVER, stat).
+
+set(Key, Value) ->
+  set(Key, Value, [{expiry, 0}, {cas, 0}]).
+set(Key, Value, Options) ->
+  gen_server:call(?SERVER, {set, Key, Value, Options}).
+
+get(Key) ->
+  gen_server:call(?SERVER, {get, Key}).
+
+delete(Key) ->
+  gen_server:call(?SERVER, {delete, Key}).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init([Host, Port]) ->
+  gen_tcp:connect(Host, Port, ?TCP_OPTS).
+
+handle_call(version, _From, Socket) ->
+  Response = case send(Socket, whisk_operation:request(?OP_VERSION)) of
+    {ok, Data} -> whisk_operation:response(Data);
+    E -> E
+  end,
+  {reply, Response, Socket};
+handle_call(stat, _From, Socket) ->
+  Response = case send(multi, ?OP_STAT, Socket, whisk_operation:request(?OP_STAT)) of
+    {ok, Data} -> whisk_operation:response(Data);
+    E -> E
+  end,
+  {reply, Response, Socket};
+handle_call({set, Key, Value, Options}, _From, Socket) ->
+  Expiry = elists:keyfind(expiry, 1, Options, 0),
+  CAS = elists:keyfind(cas, 1, Options, 0),
+  {Flags, Value1} = if
+                      is_binary(Value) -> {0, Value};
+                      true -> {1, term_to_binary(Value)}
+                    end,
+  Extra = <<Flags:32, Expiry:32>>,
+  Response = case send(Socket, whisk_operation:request(?OP_SET, [{extra, Extra},
+                                                                 {key, eutils:to_binary(Key)},
+                                                                 {value, Value1},
+                                                                 {cas, CAS}])) of
+               {ok, Data} -> whisk_operation:response(Data);
+               E -> E
+             end,
+  {reply, Response, Socket};
+handle_call({get, Key}, _From, Socket) ->
+  Response = case send(Socket, whisk_operation:request(?OP_GET, [{key, eutils:to_binary(Key)}])) of
+               {ok, Data} -> whisk_operation:response(Data);
+               E -> E
+             end,
+  {reply, Response, Socket};
+handle_call({delete, Key}, _From, Socket) ->
+  Response = case send(Socket, whisk_operation:request(?OP_DELETE, [{key, eutils:to_binary(Key)}])) of
+               {ok, Data} -> whisk_operation:response(Data);
+               E -> E
+             end,
+  {reply, Response, Socket};
+handle_call(_Request, _From, Socket) ->
+  {reply, ok, Socket}.
+
+handle_cast(stop, Socket) ->
+  {stop, normal, Socket};
+handle_cast(_Msg, Socket) ->
+  {noreply, Socket}.
+
+handle_info(_Info, Socket) ->
+  {noreply, Socket}.
+
+terminate(_Reason, _Socket) ->
+  ok.
+
+code_change(_OldVsn, Socket, _Extra) ->
+  {ok, Socket}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+send(Socket, Request) ->
+  send(simple, 0, Socket, Request).
+
+send(Mode, Operation, Socket, Request) ->
+  case gen_tcp:send(Socket, Request) of
+    ok -> 
+      if 
+        Mode =:= multi -> multi_recv(Operation);
+        true -> simple_recv()
+      end;
+    E -> E
+  end.
+
+simple_recv() ->
+  receive
+    {tcp, _, Data} ->
+      {ok, Data};
+    {error, E} ->
+      {error, E}
+  after ?TIMEOUT -> 
+          {error, timeout}
+  end.
+
+multi_recv(Operation) ->
+  multi_recv(Operation, []).
+
+multi_recv(Operation, Data) ->
+  receive
+    {tcp, _, NewData} ->
+      case whisk_operation:is_end(Operation, NewData) of
+        true -> {ok, Data};
+        false -> multi_recv(Operation, [NewData|Data])
+      end;
+    {error, E} ->
+      {error, E}
+  after ?TIMEOUT ->
+          {error, timeout}
+  end.
---
+++ src/whisk_operation.erl
@@ -0,0 +1,131 @@
+-module(whisk_operation).
+-include("../include/whisk.hrl").
+
+-export([
+         request/1,
+         request/2,
+         is_end/2,
+         response/1
+        ]).
+
+request(Operation) ->
+  request(Operation, []).
+
+request(Operation, Datas) ->
+  Extra = eutils:to_binary(elists:keyfind(extra, 1, Datas, <<>>)),
+  Key = eutils:to_binary(elists:keyfind(key, 1, Datas, <<>>)),
+  Value = eutils:to_binary(elists:keyfind(value, 1, Datas, <<>>)),
+  Opaque = elists:keyfind(opaque, 1, Datas, 0),
+  CAS = elists:keyfind(cas, 1, Datas, 0),
+  <<?REQUEST, 
+    Operation, 
+    (size(Key)):16, 
+    (size(Extra)), 
+    0, 
+    0:16, 
+    (size(Extra)+size(Key)+size(Value)):32,
+    Opaque:32,
+    CAS:64,
+    Extra/binary,
+    Key/binary,
+    Value/binary>>.
+
+is_end(Operation, <<?RESPONSE,
+                    Operation,
+                    0:16,
+                    0,
+                    0,
+                    ?STATUS_NO_ERROR:16,
+                    0:32,
+                    0:32,
+                    0:64>>) ->
+  true;
+is_end(_, _) ->
+  false.
+
+% Version
+response(<<?RESPONSE,
+           ?OP_VERSION,
+           0:16,
+           0,
+           0,
+           ?STATUS_NO_ERROR:16,
+           Size:32,
+           0:32,
+           0:64,
+           Version/binary>>) when size(Version) =:= Size ->
+  {ok, Version};
+% Stat
+response(<<?RESPONSE,
+           ?OP_STAT,
+           KeyLength:16,
+           0,
+           0,
+           ?STATUS_NO_ERROR:16,
+           Size:32,
+           0:32,
+           0:64,
+           Key:KeyLength/binary,
+           Value/binary>>) when size(Key) + size(Value) =:= Size ->
+  {ok, {Key, Value}};
+% Set
+response(<<?RESPONSE,
+           ?OP_SET,
+           0:16,
+           0,
+           0,
+           ?STATUS_NO_ERROR:16,
+           0:32,
+           0:32,
+           CAS:64>>) ->
+  {ok, CAS};
+% Get
+response(<<?RESPONSE,
+           ?OP_GET,
+           0:16,
+           ExtraLength,
+           0,
+           ?STATUS_NO_ERROR:16,
+           BodyLength:32,
+           0:32,
+           _:64,
+           Flags:32,
+           Body/binary>>) when ExtraLength =:= 4, size(Body) =:= BodyLength - ExtraLength ->
+  if
+    Flags =:= 1 -> {ok, binary_to_term(Body)};
+    true -> {ok, Body}
+  end;
+% Delete
+response(<<?RESPONSE,
+           ?OP_DELETE,
+           0:16,
+           0,
+           0,
+           ?STATUS_NO_ERROR:16,
+           0:32,
+           0:32,
+           0:64>>) ->
+  ok;
+% Error
+response(<<?RESPONSE,
+           _,
+           0:16,
+           0,
+           0,
+           Status:16,
+           0:32,
+           0:32,
+           _:64>>) when Status =/= ?STATUS_NO_ERROR ->
+  {error, Status};
+response(Data) when is_list(Data) ->
+  lists:foldl(fun
+                (D, {ok, Acc}) ->
+                  case response(D) of
+                    {ok, Response} -> {ok, [Response|Acc]};
+                    {error, E} -> {error, E}
+                  end;
+                (_, Acc) -> Acc
+              end, {ok, []}, Data);
+response(_) ->
+  {error, invalid_response}.
+
