c47a05ac70a0f73d4374fb38f8bd67939b7d5c4b
SBfrr <sebastien@ferre.io>
2016-04-13 13:25:47 +0200

    Mix version is expected to be a SemVer version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Whisk.Mixfile do
 
 	def project do
 		[app: :whisk,
-		 version: "1",
+		 version: "1.0.0",
 		 elixir: "~> 1.2",
 		 build_embedded: Mix.env == :prod,
 		 start_permanent: Mix.env == :prod,
